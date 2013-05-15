0b071e88710de07d0a7b789e596994b9bc7f3e73
jribeiro <jribeiro@ciandt.com>
2013-05-14 22:20:44 -0300

    Testing google oauth

--- deploy/app/controllers/oauth_controller.php
+++ deploy/app/controllers/oauth_controller.php
@@ -17,34 +17,50 @@ class OauthController extends AppController {
     $client->setClientId('276422030021.apps.googleusercontent.com');
     $client->setClientSecret('r-8c7keF-9FWs89VDPvQR600');
     $client->setRedirectUri('http://cit.hnus.webfactional.com/oauth');
-//     $client->setDeveloperKey('insert_your_simple_api_key');
     $plus = new Google_PlusService($client);
     
-    if (isset($_GET['code'])) {
-      $client->authenticate();
-      $_SESSION['token'] = $client->getAccessToken();
-      $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-      header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
+    if (isset($_REQUEST['logout'])) {
+      unset($_SESSION['access_token']);
     }
     
-    if (isset($_SESSION['token'])) {
-      $client->setAccessToken($_SESSION['token']);
+    if (isset($_GET['code'])) {
+      $client->authenticate($_GET['code']);
+      $_SESSION['access_token'] = $client->getAccessToken();
+      header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
+    }
+    
+    if (isset($_SESSION['access_token'])) {
+      $client->setAccessToken($_SESSION['access_token']);
     }
     
     if ($client->getAccessToken()) {
-      $activities = $plus->activities->listActivities('me', 'public');
-      print 'Your Activities: <pre>' . print_r($activities, true) . '</pre>';
+      $me = $plus->people->get('me');
     
-      // We're not done yet. Remember to update the cached access token.
-      // Remember to replace $_SESSION with a real database or memcached.
-      $_SESSION['token'] = $client->getAccessToken();
+      // These fields are currently filtered through the PHP sanitize filters.
+      // See http://www.php.net/manual/en/filter.filters.sanitize.php
+      $url = filter_var($me['url'], FILTER_VALIDATE_URL);
+      $img = filter_var($me['image']['url'], FILTER_VALIDATE_URL);
+      $name = filter_var($me['displayName'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+      $personMarkup = "<a rel='me' href='$url'>$name</a><div><img src='$img'></div>";
+    
+      $optParams = array('maxResults' => 100);
+      $activities = $plus->activities->listActivities('me', 'public', $optParams);
+      $activityMarkup = '';
+      foreach($activities['items'] as $activity) {
+        // These fields are currently filtered through the PHP sanitize filters.
+        // See http://www.php.net/manual/en/filter.filters.sanitize.php
+        $url = filter_var($activity['url'], FILTER_VALIDATE_URL);
+        $title = filter_var($activity['title'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+        $content = filter_var($activity['object']['content'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+        $activityMarkup .= "<div class='activity'><a href='$url'>$title</a><div>$content</div></div>";
+      }
+    
+      // The access token may have been updated lazily.
+      $_SESSION['access_token'] = $client->getAccessToken();
     } else {
       $authUrl = $client->createAuthUrl();
-      print "<a href='$authUrl'>Connect Me!</a>";
     }
     
-    
-  }
 
 }
 ?>
