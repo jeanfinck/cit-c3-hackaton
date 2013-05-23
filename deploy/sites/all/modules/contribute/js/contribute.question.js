b01d56f9ff8abcbc9200c2a7e4836fb5f28ce4ca
Lucas Nascimento Arruda <lucas@ciandt.com>
2013-05-23 00:15:10 -0300

    Database dump updated. Modules elements and jquery_update added. Advice page form built. Description blocks added.

--- deploy/sites/all/modules/contribute/js/contribute.question.js
+++ deploy/sites/all/modules/contribute/js/contribute.question.js
@@ -1,1 +1,22 @@
-Drupal.settings.ckeditor.input_formats.minimal.show_toggle = 'f';
+jQuery(function() {
+  initSideBarBlocks();
+});
+
+function initSideBarBlocks() {
+  jQuery('div.region-sidebar-second .block').hide();
+
+  jQuery('#edit-title').focus(function(){
+    jQuery('div.region-sidebar-second .block').hide();
+    jQuery('#block-block-1').fadeIn();
+  });
+
+  CKEDITOR.instances["edit-question-value"].on('focus', function(e) {
+    jQuery('div.region-sidebar-second .block').hide();
+    jQuery('#block-block-2').fadeIn();
+  });
+
+  jQuery('#edit-term-entry').focus(function(){
+    jQuery('div.region-sidebar-second .block').hide();
+    jQuery('#block-block-3').fadeIn();
+  });
+}
