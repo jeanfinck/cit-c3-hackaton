d5b169f69fefb02851a14f3b35eef9ffce4e0149
jribeiro <jribeiro@ciandt.com>
2013-05-23 01:08:12 -0300

    Added functionality to user can answer a question

--- deploy/sites/all/themes/sky/templates/node--question.tpl.php
+++ deploy/sites/all/themes/sky/templates/node--question.tpl.php
@@ -1,20 +1,17 @@
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print $unpublished; ?>
-
   <?php print render($title_prefix); ?>
   <?php print render($title_suffix); ?>
 
   <div<?php print $content_attributes; ?>>
 
     <div class="question">
-      <?php
-      print render($content['body']);?>
+      <?php print render($content['body']);?>
     </div>
 
     <div class="user-data">
       <?php print render($content['field_question_user']);?>
-
       <?php if (!empty($user_picture)): ?>
         <div class="user-picture"><?php print $user_picture; ?></div>
       <?php endif; ?>
@@ -30,7 +27,37 @@
     </div>
 
     <div class="answers">
-
+      <h2><?php print count($answers);?> Answers</h2>
+      <!-- Correct Answer -->
+      <?php if (!empty($correct_answer)):?>
+      <div class="answer answer-nid-<?php print $correct_answer->nid;?> correct">
+        <div class="vote">
+          <a title="This answer is useful" class="vote-up-off">up vote</a>
+          <span class="vote-count-post ">
+          <?php print render(field_view_field('node', $correct_answer, 'field_answer_votes', array('label'=>'hidden')));?>
+          </span>
+          <a title="This answer is not useful" class="vote-down-off">down vote</a>
+          <span class="vote-accepted-on">accepted</span>
+        </div>
+        
+        <?php print render(field_view_field('node', $correct_answer, 'body', array('label'=>'hidden')));?>
+      </div>
+      <?php endif;?>
+      
+      <!-- Other Answers -->
+      <?php if (!empty($correct_answer)){ unset($answers[$correct_answer->nid]); } // Remove correct answer from the normal list
+      foreach ($answers as $nid => $answer_node): ?>
+      <div class="answer answer-nid-<?php print $nid;?>">
+        <div class="vote">
+          <a title="This answer is useful" class="vote-up-off">up vote</a>
+          <span class="vote-count-post ">
+          <?php print render(field_view_field('node', $answer_node, 'field_answer_votes', array('label'=>'hidden')));?>
+          </span>
+          <a title="This answer is not useful" class="vote-down-off">down vote</a>
+        </div>
+        <?php print render(field_view_field('node', $answer_node, 'body', array('label'=>'hidden')));?>
+      </div>
+      <?php endforeach; ?>
     </div>
 
   </div>
