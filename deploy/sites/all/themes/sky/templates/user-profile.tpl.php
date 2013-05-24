c5bdd5e1e65f817e05f973a206c52adca9f7bf16
Gilberto Crespo <gilcrespo@gmail.com>
2013-05-24 16:26:58 -0300

    profile page

--- deploy/sites/all/themes/sky/templates/user-profile.tpl.php
+++ deploy/sites/all/themes/sky/templates/user-profile.tpl.php
@@ -1,3 +1,39 @@
+<div class="prof-blocks">
+<h1>nome do colaborador</h1>
+<div class="prof-image">profile image</div>
+<div class="prof-bio">
+  <label>bio</label><p>TEXTO</p>
+  <label>status</label><p>STATUS</p>
+</div>
+</div>
+<div class="prof-blocks">
+<h2>Achievements</h2>
+<ul>
+	<li><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-commenter.png" /> <span> <img class="callout" src="/sites/default/files/public/badges/callout.gif" />
+	<strong>Commenter</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+	<li><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-interaction.png" /> <span> <img class="callout" src="/sites/default/files/public/badges/callout.gif" />
+	<strong>interaction</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+	<li><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-mastering.png" /> <span> <img class="callout" src="/sites/default/files/public/badges/callout.gif" />
+	<strong>mastering</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+	<li><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-contributor.png" /> <span> <img class="callout" src="/sites/default/files/public/badges/callout.gif" />
+	<strong>contributor</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+	<li><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-wise.png" /> <span> <img class="callout" src="/sites/default/files/public/badges/callout.gif" />
+	<strong>wise</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+	<li><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-questioner.png" /> <span> <img class="callout" src="/sites/default/files/public/badges/callout.gif" />
+	<strong>questioner</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+	<li><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-favorite.png" /> <span> <img class="callout" src="/sites/default/files/public/badges/callout.gif" />
+	<strong>favorite</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+	<li><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-achiever.png" /> <span> <img class="callout" src="/sites/default/files/public/badges/callout.gif" />
+	<strong>achiever</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+</ul>
+</div>
+<div class="prof-blocks">
+<h2>Last 10 Questions</h2>
+</div>
+<div class="prof-blocks">
+<h2>Last 10 Answers</h2>
+</div>
+
 <?php
 
 var_dump($user_profile);
