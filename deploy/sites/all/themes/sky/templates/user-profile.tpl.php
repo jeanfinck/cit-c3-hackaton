a656667deb9c2f76fdcb7ef8e3903748d216e3cd
handrus <handrus@ciandt.com>
2013-05-24 17:38:01 -0300

    Revert "profile page"
    This reverts commit eb81a6300fa27bb7e54f4bf3cffd7e5ffac730fb.

---
+++ deploy/sites/all/themes/sky/templates/user-profile.tpl.php
@@ -0,0 +1,47 @@
+<div class="prof-blocks">
+<h1>Gilberto Carlos Crespo Júnior</h1>
+<table>
+	<tr>
+		<td rowspan="2">profile image</td>
+		<td align="right"><em class="labeling">bio</em></td>
+		<td><small>uiasBfsd uifsdb fuiuisbd fuisdbfui sdbuif bsdui bfuisduifbsd uifsuidb fuisd uif bdfuisdb uiui sdui bfuisdb fuisbd uifbsduib fuisd bfuisdbui</small></td>
+	</tr>
+	<tr>
+		<td><em class="labeling">status</em></td>
+		<td>STATUS</td>
+	</tr>
+	</table>
+</div>
+
+<div class="prof-blocks">
+<h2>Achievements</h2>
+<ul>
+	<li><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-commenter.png" /><span>
+	<strong>Commenter</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+	<li><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-interaction.png" /><span>
+	<strong>First Interaction</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+	<li class="inactive"><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-mastering.png" /><span>
+	<strong>Technology Mastering</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+	<li class="inactive"><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-contributor.png" /><span>
+	<strong>Contributor Member</strong><br /> Be a part of the team</span></a></li>
+	<li class="inactive"><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-wise.png" /><span>
+	<strong>The Wise</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+	<li><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-questioner.png" /><span>
+	<strong>Questioner</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+	<li><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-favorite.png" /><span>
+	<strong>The Favorite</strong><br /> Pure CSS popup tooltips with clean semantic XHTML.</span></a></li>
+	<li class="inactive"><a href="#" class="tooltip"> <img src="/sites/default/files/public/badges/badge-achiever.png" /><span>
+	<strong>Achiever</strong><br /> Collect at least 7 badges</span></a></li>
+</ul>
+</div>
+<br class="clear" />
+<div class="prof-blocks">
+<h2>Last 10 Questions</h2>
+</div>
+<div class="prof-blocks">
+<h2>Last 10 Answers</h2>
+</div>
+
+<?php
+
+var_dump($user_profile);
