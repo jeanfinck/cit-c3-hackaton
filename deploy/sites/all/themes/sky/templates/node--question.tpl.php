b662c8f150346d2fc80dde7b62a3173929a93ca1
jkribeiro <jribeiro@ciandt.com>
2013-06-28 16:36:23 -0300

    Added vote rigth answer functionality

--- deploy/sites/all/themes/sky/templates/node--question.tpl.php
+++ deploy/sites/all/themes/sky/templates/node--question.tpl.php
@@ -39,48 +39,68 @@
     <div class="answers">
       <h2 clas="answer-count"><?php print count($answers);?> Answers</h2>
 
-      <!-- Correct Answer -->
-      <?php if (!empty($correct_answer)):?>
-      <div class="answer answer-nid-<?php print $correct_answer->nid;?> correct">
+      <!-- Right Answer -->
+      <?php if (!empty($right_answer)):?>
+      <div class="answer answer-nid-<?php print $right_answer->nid;?> right">
 
-        <!-- Answers Vote -->
+        <?php if ($right_answer->current_user_already_voted):?>
+        <div class="already-voted">You voted for this Answer</div>
+        <?php endif;?>
+
+        <!-- Answer Vote -->
         <div class="vote">
-          <input type="hidden" value="<?php print $correct_answer->nid;?>">
+          <input type="hidden" value="<?php print $right_answer->nid;?>">
           <a title="This answer is useful" class="vote-up">up vote</a>
-          <span class="vote-count-post">
-            <?php print $node->total_votes;?>
-          </span>
+          <span class="vote-count-post"><?php print $right_answer->total_votes;?></span>
           <a title="This answer is not useful" class="vote-down">down vote</a>
           <span class="vote-accepted-on">accepted</span>
         </div>
         <!-- [End] Answers Vote -->
 
-        <?php print render(field_view_field('node', $correct_answer, 'body', array('label'=>'hidden')));?>
+        <!-- User data -->
+        <div class="user-data">
+          <span class="time">answered <?php print date('D, d M Y H:i:s', $right_answer->created)?></span>
+          <div class="author">
+            <?php $user_image_path = isset($right_answer->author_details->picture->uri) ? $right_answer->author_details->picture->uri : 'public://pictures/default_user_avatar.png';
+              print theme('image', array(
+                      'path' => $user_image_path,
+                      'title' => $right_answer->author_details->name,
+                      'width' => '32px',
+                      'attributes' => array('align' => 'left', 'hspace' => '10'),
+                    ));?>
+            <?php print l($right_answer->author_details->name, 'user/' . $right_answer->author_details->uid, array('attributes' => array('target'=>'_blank')));?><br/>
+            <?php print $right_answer->author_details->mail;?>
+          </div>
+        </div>
+        <!-- [End] User data -->
+
+        <?php print render(field_view_field('node', $right_answer, 'body', array('label'=>'hidden')));?>
       </div>
       <?php endif;?>
-      <!-- [End] Correct Answer -->
+      <!-- [End] Right Answer -->
 
       <!-- Other Answers -->
-      <?php if (!empty($correct_answer)){ unset($answers[$correct_answer->nid]); } // Remove correct answer from the normal list
+      <?php if (!empty($right_answer)){ unset($answers[$right_answer->nid]); } // Remove right answer from the normal list
               foreach ($answers as $nid => $answer_node):?>
       <div class="answer answer-nid-<?php print $nid;?>">
+
         <?php if ($answer_node->current_user_already_voted):?>
         <div class="already-voted">You voted for this Answer</div>
         <?php endif;?>
 
-        <!-- Answers Vote -->
+        <!-- Answer Vote -->
         <div class="vote">
           <input type="hidden" value="<?php print $nid;?>">
           <a title="This answer is useful" class="vote-up">up vote</a>
-          <span class="vote-count-post ">
-          <?php print $answer_node->total_votes;?>
-          </span>
+          <span class="vote-count-post "><?php print $answer_node->total_votes;?></span>
           <a title="This answer is not useful" class="vote-down">down vote</a>
+          <?php global $user; if (($node->uid == $user->uid) && empty($right_answer)):?>
+          <a title="Select Correct Answer" class="right-answer" href="/post/answer/right/<?php print $node->nid;?>/<?php print $answer_node->nid;?>">Right Answer</a>
+          <?php endif;?>
         </div>
         <!-- [End] Answers Vote -->
 
-        <?php $element = field_view_field('node', $answer_node, 'body', array('label'=>'hidden'));
-          print render($element);?>
+        <?php print render(field_view_field('node', $answer_node, 'body', array('label'=>'hidden')));?>
 
         <!-- Answers User data -->
         <div class="user-data">
@@ -103,7 +123,6 @@
       <?php endforeach; ?>
     </div>
     <!-- [End] Answers -->
-
   </div>
 
   <!-- Comment -->
@@ -113,11 +132,10 @@
   <!-- [End] Comment -->
 
   <!-- New Answer -->
+  <?php global $user; if ($node->uid != $user->uid):?>
   <div class="answer new">
-    <?php
-      $element = drupal_get_form('contribute_answer_form');
-      print render($element);
-    ?>
+    <?php print render(drupal_get_form('contribute_answer_form')); ?>
   </div>
+  <?php endif;?>
   <!-- [End] New Answer -->
 </article>
