a1a052bcae02140b14cc2358ddb3fed616ed55b4
jkribeiro <jribeiro@ciandt.com>
2013-07-01 14:47:40 -0300

    Votes confirm behavior

--- deploy/sites/all/themes/sky/js/answer_votes.js
+++ deploy/sites/all/themes/sky/js/answer_votes.js
@@ -1,40 +1,101 @@
 // $Id$
 (function ($) {
   $(document).ready(function() {
-
-    // Voting UP
-    $('div.answer div.vote a.vote-up').click(function(){
-      var answerNid = $('input', $(this).parent()).val();
-      var questionNid = Drupal.settings.contribute.questionNid;
-      sendNewAnswerVote(questionNid, answerNid, 'up');
-    });
-    
-    // Voting Down
-    $('div.answer div.vote a.vote-down').click(function(){
-      var answerNid = $('input', $(this).parent()).val();
-      var questionNid = Drupal.settings.contribute.questionNid;
-      sendNewAnswerVote(questionNid, answerNid, 'down');
-    });
-
-    function sendNewAnswerVote(questionNid, answerNid, voteType) {
-      var posting = $.post('/post/answer/vote/' + questionNid + '/' + answerNid + '/' + voteType);
-
-      posting.done(function(data) {
-        var returnedDataObj = JSON.parse(data);
-        
-        if (returnedDataObj.status == 'success') {
-          // Update votes number
-          $('div.answer-nid-' + returnedDataObj.answer + ' span.vote-count-post').html(returnedDataObj.votes);
-        }
-        
-        // Display Glow message
-        $.gritter.add({
-          title: returnedDataObj.title,
-          text: returnedDataObj.message
-        });
-        
-      });
-    };
-    
+    answerVotesInit();
+    bestAnswerInit();
   });
+
+  /**
+   * Implements answer votes behavior
+   */
+  function answerVotesInit() {
+    $('div.answer div.vote a.vote-up, div.answer div.vote a.vote-down').click(function(){
+      var voteType = $(this).attr('class');
+      var answerNid = $('input', $(this).parent()).val();
+      var questionNid = Drupal.settings.contribute.questionNid;
+      var dialogTitle = 'Are you sure?';
+      var dialogMessage = 'Are you sure you want to make this vote?';
+      
+      newAnswerVote(questionNid, answerNid, voteType, dialogTitle, dialogMessage);
+    });
+  }
+  
+  /**
+   * New answer vote behavior
+   */
+  function newAnswerVote(questionNid, answerNid, voteType, dialogTitle, dialogMessage) {
+    $('body').append('<div id="dialog-confirm"><p>' + Drupal.t(dialogMessage) + '</p></div>');
+    
+    $("div#dialog-confirm").dialog({
+      resizable: false,
+      modal: true,
+      draggable: false,
+      title: Drupal.t(dialogTitle),
+      buttons: {
+        'OK': function() {
+          $(this).dialog('close');
+          $(this).remove();
+          sendAnswerVoteRequest(questionNid, answerNid, voteType);
+        },
+        Cancel: function() {
+          $(this).dialog('close');
+          $(this).remove();
+        }
+      }
+    });
+  }
+  
+  /**
+   * Send a post request to save the user vote
+   */
+  function sendAnswerVoteRequest(questionNid, answerNid, voteType) {
+    var posting = $.post('/post/answer/vote/' + questionNid + '/' + answerNid + '/' + voteType);
+
+    posting.done(function(data) {
+      var returnedDataObj = JSON.parse(data);
+      
+      if (returnedDataObj.status == 'success') {
+        // Dynamically update votes number
+        $('div.answer-nid-' + returnedDataObj.answer + ' span.vote-count-post').html(returnedDataObj.votes);
+      }
+      
+      // Display Glow message
+      $.gritter.add({
+        title: returnedDataObj.title,
+        text: returnedDataObj.message
+      });
+      
+    });
+  };
+  
+  /**
+   * Select Best Answer Behavior
+   */
+  function bestAnswerInit() {
+    $('div.vote a.best-answer').click(function(e){
+      e.preventDefault();
+      
+      var requestURL = $(this).attr('href');
+      
+      $('body').append('<div id="dialog-confirm"><p>' + Drupal.t('Are you sure you want to mark this answer as the best solution?') + '</p></div>');
+      
+      $("div#dialog-confirm").dialog({
+        resizable: false,
+        modal: true,
+        draggable: false,
+        title: Drupal.t('Are you sure?'),
+        buttons: {
+          'OK': function() {
+            $(this).dialog('close');
+            $(this).remove();
+            window.location.href = requestURL;
+          },
+          Cancel: function() {
+            $(this).dialog('close');
+            $(this).remove();
+          }
+        }
+      });
+    });
+  }
 })(jQuery);
--- deploy/sites/all/themes/sky/templates/node--question.tpl.php
+++ deploy/sites/all/themes/sky/templates/node--question.tpl.php
@@ -35,52 +35,52 @@
     </div>
     <!-- [End] Tag and categories -->
 
-    <!-- Answers -->
+    <!-- Answers Container -->
     <div class="answers">
       <h2 clas="answer-count"><?php print count($answers);?> Answers</h2>
 
-      <!-- Right Answer -->
-      <?php if (!empty($right_answer)):?>
-      <div class="answer answer-nid-<?php print $right_answer->nid;?> right">
+      <!-- Best Answer Selected -->
+      <?php if (!empty($best_answer)):?>
+      <div class="answer answer-nid-<?php print $best_answer->nid;?> best">
 
-        <?php if ($right_answer->current_user_already_voted):?>
+        <?php if ($best_answer->current_user_already_voted):?>
         <div class="already-voted">You voted for this Answer</div>
         <?php endif;?>
 
-        <!-- Answer Vote -->
+        <!-- Vote -->
         <div class="vote">
-          <input type="hidden" value="<?php print $right_answer->nid;?>">
+          <input type="hidden" value="<?php print $best_answer->nid;?>">
           <a title="This answer is useful" class="vote-up">up vote</a>
-          <span class="vote-count-post"><?php print $right_answer->total_votes;?></span>
+          <span class="vote-count-post"><?php print $best_answer->total_votes;?></span>
           <a title="This answer is not useful" class="vote-down">down vote</a>
-          <span class="vote-accepted-on">accepted</span>
+          <span class="vote-accepted-on" title="The question owner accepted this as the best answer">accepted</span>
         </div>
-        <!-- [End] Answers Vote -->
+        <!-- [End] Vote -->
 
         <!-- User data -->
         <div class="user-data">
-          <span class="time">answered <?php print date('D, d M Y H:i:s', $right_answer->created)?></span>
+          <span class="time">answered <?php print date('D, d M Y H:i:s', $best_answer->created)?></span>
           <div class="author">
-            <?php $user_image_path = isset($right_answer->author_details->picture->uri) ? $right_answer->author_details->picture->uri : 'public://pictures/default_user_avatar.png';
+            <?php $user_image_path = isset($best_answer->author_details->picture->uri) ? $best_answer->author_details->picture->uri : 'public://pictures/default_user_avatar.png';
               print theme('image', array(
                       'path' => $user_image_path,
-                      'title' => $right_answer->author_details->name,
+                      'title' => $best_answer->author_details->name,
                       'width' => '32px',
                       'attributes' => array('align' => 'left', 'hspace' => '10'),
                     ));?>
-            <?php print l($right_answer->author_details->name, 'user/' . $right_answer->author_details->uid, array('attributes' => array('target'=>'_blank')));?><br/>
-            <?php print $right_answer->author_details->mail;?>
+            <?php print l($best_answer->author_details->name, 'user/' . $best_answer->author_details->uid, array('attributes' => array('target'=>'_blank')));?><br/>
+            <?php print $best_answer->author_details->mail;?>
           </div>
         </div>
         <!-- [End] User data -->
 
-        <?php print render(field_view_field('node', $right_answer, 'body', array('label'=>'hidden')));?>
+        <?php print render(field_view_field('node', $best_answer, 'body', array('label'=>'hidden')));?>
       </div>
       <?php endif;?>
-      <!-- [End] Right Answer -->
+      <!-- [End] Best Answer Selected -->
 
       <!-- Other Answers -->
-      <?php if (!empty($right_answer)){ unset($answers[$right_answer->nid]); } // Remove right answer from the normal list
+      <?php if (!empty($best_answer)){ unset($answers[$best_answer->nid]); } // Remove best answer from the normal list
               foreach ($answers as $nid => $answer_node):?>
       <div class="answer answer-nid-<?php print $nid;?>">
 
@@ -88,21 +88,21 @@
         <div class="already-voted">You voted for this Answer</div>
         <?php endif;?>
 
-        <!-- Answer Vote -->
+        <!-- Vote -->
         <div class="vote">
           <input type="hidden" value="<?php print $nid;?>">
           <a title="This answer is useful" class="vote-up">up vote</a>
           <span class="vote-count-post "><?php print $answer_node->total_votes;?></span>
           <a title="This answer is not useful" class="vote-down">down vote</a>
-          <?php global $user; if (($node->uid == $user->uid) && empty($right_answer)):?>
-          <a title="Select Correct Answer" class="right-answer" href="/post/answer/right/<?php print $node->nid;?>/<?php print $answer_node->nid;?>">Right Answer</a>
+          <?php global $user; if (($node->uid == $user->uid) && empty($best_answer)):?>
+          <a title="Select the best Answer" class="best-answer" href="/post/answer/best/<?php print $node->nid;?>/<?php print $answer_node->nid;?>">Best Answer</a>
           <?php endif;?>
         </div>
-        <!-- [End] Answers Vote -->
+        <!-- [End] Vote -->
 
         <?php print render(field_view_field('node', $answer_node, 'body', array('label'=>'hidden')));?>
 
-        <!-- Answers User data -->
+        <!-- User data -->
         <div class="user-data">
           <span class="time">answered <?php print date('D, d M Y H:i:s', $answer_node->created)?></span>
           <div class="author">
@@ -117,12 +117,12 @@
             <?php print $answer_node->author_details->mail;?>
           </div>
         </div>
-        <!-- [End] Answers User data -->
+        <!-- [End] User data -->
 
       </div>
       <?php endforeach; ?>
     </div>
-    <!-- [End] Answers -->
+    <!-- [End] Answers Container -->
   </div>
 
   <!-- Comment -->
